ca5d6791e8619311f998302193d68ad1d2dcb6c7
ibdkn <ibudiakina@gmail.com>
2024-03-11 22:33:43 +0300

    added media to Theme.tsx

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -6,5 +6,9 @@ export const theme = {
 
         primaryColor: "#ffffff",
         secondaryColor: "#abb2bf"
+    },
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
     }
 }
