c4a7217e3212ae53ffb5c48cb9b95c359c31bb68
ibdkn <ibudiakina@gmail.com>
2024-03-11 22:34:59 +0300

    added switch to menu and delete from header

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -6,19 +6,29 @@ import { theme } from '../../styles/Theme';
 
 export const Switch = () => {
     return (
-        <FlexWrapper align={"center"} gap={"4px"}>
+        <SwitchStyled>
             <Language>EN</Language>
             <Icon iconId={'SwitchArrow'} width={'30'} height={'30'} viewBox={"0 -7 30 30"}/>
-        </FlexWrapper>
+        </SwitchStyled>
     );
 };
 
-const Language = styled.p`
-  color: ${theme.colors.secondaryColor};
+const SwitchStyled = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
   cursor: pointer;
   
-  &:hover {
+  svg {
+    color: ${theme.colors.secondaryColor};
+  }
+
+  &:hover p, &:hover svg {
     color: ${theme.colors.primaryColor};
   }
+`
+
+const Language = styled.p`
+  color: ${theme.colors.secondaryColor};
 `
 
