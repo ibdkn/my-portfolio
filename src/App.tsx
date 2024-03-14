e8b6ed84659113187644cddc3a964a748fa3740a
ibdkn <ibudiakina@gmail.com>
2024-03-14 21:18:12 +0300

    added tag main

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,14 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <Main />
-            <Quote />
-            <Projects />
-            <Skills />
-            <About />
-            <Contacts />
+            <main>
+                <Main />
+                <Quote />
+                <Projects />
+                <Skills />
+                <About />
+                <Contacts />
+            </main>
             <Footer />
         </div>
     );
