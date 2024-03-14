6903231be4f50cd44ba9a194639203f32ba24d90
ibdkn <ibudiakina@gmail.com>
2024-03-14 21:16:13 +0300

    added mixin for button's font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const StyledButton = styled.button`
   padding: 8px 16px;
   width: max-content;
   min-width: 109px;
   height: 37px;
-  font-weight: 500;
-  font-size: 16px;
+  ${font({weight: 500, Fmax: 16, Fmin: 14})};
   background: transparent;
   border: 1px solid ${theme.colors.accent};
   
