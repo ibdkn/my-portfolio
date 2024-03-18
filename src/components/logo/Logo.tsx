06af701a03977d8dcd464539e500a5f8a3fc267c
ibdkn <ibudiakina@gmail.com>
2024-03-18 23:53:25 +0300

    added scroll to top

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Icon} from "../icon/Icon";
 import {S} from "./Logo_Styles"
+import {animateScroll as scroll} from "react-scroll";
 
 export const Logo: React.FC = () => {
     return (
-        <S.LogoWrapper href={"#"}>
+        <S.LogoWrapper onClick={() => {scroll.scrollToTop()}}>
             <Icon iconId={'logo'} width={'16'} height={'16'}/>
             <S.LogoName>Bdkni</S.LogoName>
         </S.LogoWrapper>
--- src/components/logo/Logo_Styles.ts
+++ src/components/logo/Logo_Styles.ts
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 const LogoWrapper = styled.a`
   position: relative;
-  z-index: 999999999;
+  z-index: 999999;
   display: flex;
   align-items: center;
   gap: 8px;
+  cursor: pointer;
 `
 
 const LogoName = styled.span`
