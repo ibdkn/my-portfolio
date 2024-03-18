d29c1fb7afbee8d33b458e2d9a3555f7f4c839bd
ibdkn <ibudiakina@gmail.com>
2024-03-18 16:07:38 +0300

    added component for Info

---
+++ src/pages/about/info/Info.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Description} from "../../../components/about/description/Description";
+import {Photo} from "../../../components/about/photo/Photo";
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+export const Info = () => {
+    return (
+        <StyledInfo>
+            <FlexWrapper justify={"space-between"} align={"center"}>
+                <FlexWrapper direction={"column"}>
+                    <Description/>
+                </FlexWrapper>
+                <Photo/>
+            </FlexWrapper>
+        </StyledInfo>
+    );
+};
+
+const StyledInfo = styled.div`
+  //padding: 35px 0;
+
+  > ${FlexWrapper} {
+    @media ${theme.media.tablet} {
+      flex-direction: column-reverse;
+      justify-content: center;
+      gap: 20px;
+    }
+  }
+  
+  
+`
