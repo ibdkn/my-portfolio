d5c111b87e79a31e220cd440c5f05b0fdb87f40e
ibdkn <ibudiakina@gmail.com>
2024-03-18 16:09:29 +0300

    added styles

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme";
 
 export const Container = styled.div`
-  max-width: 1140px;
+  max-width: 1160px;
   width: 100%;
   min-height: 100%;
   padding: 0 15px;
   margin: 0 auto;
+  
+  @media ${theme.media.tablet} {
+    padding: 0 10px;
+  }
 `
